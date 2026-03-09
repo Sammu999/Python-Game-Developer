ed7d9e59dfce17cec5d30e1219c337a63870d98b
Sammu999 <samraathiremath@gmail.com>
2026-03-09 07:04:04 +0000

    Add enemy actors and movement to Galaga game
    Added enemy actors to the game and implemented their drawing and movement logic.

--- galaga.py
+++ galaga.py
@@ -2,8 +2,29 @@ import pgzrun
 WIDTH = 800
 HEIGHT = 800
 game_state = "launch"
+
 galaga = Actor ("galaga")
 galaga.pos = (WIDTH/2, HEIGHT-50)
+
+enemies = []
+ypos = 50
+for i in range (4):
+    enemy_row = []
+    xpos = 50
+    
+    for a in range (5):
+        enemy = Actor ("enemy")
+        enemy.x = xpos
+        enemy.y = ypos
+        
+        enemy_row.append (enemy)
+        xpos += 75
+
+    ypos += 50
+
+    enemies.append (enemy_row)
+
+direction = 1
 
 
 def draw():
@@ -14,18 +35,32 @@ def draw():
     
     elif game_state == "start":
         galaga.draw()
+        for enemy_row in enemies:
+            for enemy in enemy_row:
+                enemy.draw()
+            
+
        
 
     
 
 
 def update():
+    global direction
     if keyboard.A and galaga.x >= 0:
         galaga.x -= 10
 
     if keyboard.D and galaga.x <= WIDTH:
         galaga.x += 10
     
+    if enemies [0] [-1].x >= WIDTH or enemies [0] [0].x <= 0:
+        direction*=-1
+    for  enemy_row in enemies:
+        for enemy in enemy_row:
+            enemy.x += 2*direction
+
+
+
 
 def on_key_down(key):
     global game_state
@@ -38,3 +73,8 @@ def on_key_down(key):
 
 
 pgzrun.go()
+    
+
+
+
+
